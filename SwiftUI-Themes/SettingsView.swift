cf9422c929e140e206b6ec3e111779edcc761788
maksiutenkodmytro <maksiutenko.dmytro88@gmail.com>
2026-01-03 14:00:03 -0600

    Toggles added for settingsView.

--- SwiftUI-Themes/SettingsView.swift
+++ SwiftUI-Themes/SettingsView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State var isDarkModeEnabled = false
+    @State var isLightModeEnabled = false
+    
     var body: some View {
-        Text("Settings")
+        VStack{
+            Text("Settings")
+            Toggle("Dark Mode", isOn: $isDarkModeEnabled)
+            Toggle("Light Mode", isOn: $isLightModeEnabled)
+        }
+        .padding()
     }
 }
 
